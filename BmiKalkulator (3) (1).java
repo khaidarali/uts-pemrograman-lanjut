387f3f7b397dd0777266a9b7163bef5207576177
khaidarali <152408107+khaidarali@users.noreply.github.com>
2024-05-13 10:11:06 +0700

    Add files via upload

---
+++ BmiKalkulator (3) (1).java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BmiKalkulator {
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan tinggi badan: ");
+        double tinggiBadan = input.nextDouble();
+
+        System.out.println("Masukan berat badan: ");
+        double beratBadan = input.nextDouble();
+
+        double tinggiBadanM = tinggiBadan / 100.0;
+
+        double bmi = hitungBMI(tinggiBadanM, beratBadan);
+
+        System.out.println("BMI kamu adalah: " + bmi);
+
+        
+        }
+        else if (bmi >= 18.5 && bmi <= 24.9)
+        {
+            System.out.println("Berat badan kamu normal");
+        }
+        else if (bmi >= 25 && bmi <= 29.9)
+        {
+            System.out.println("Kamu kelebihan berat badan");
+        }
+        else
+        {
+            System.out.println("Kamu obesitas");
+        }
+    }
+    private static double hitungBMI(double tinggiBadanM, double beratBadan)
+    {
+        return beratBadan / (tinggiBadanM * tinggiBadanM);
+    
